e231e447d84677a0bf91b9c2fc464195d081d2dc
Mikhail Shevchuk <cp91@mail.ru>
2023-11-26 15:21:48 +0700

    Create test assembly and create first class

---
+++ ActionMagic/Assets/Tests/ElementsTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.Entities;
+
+[TestFixture]
+[Category("ECS Tests for Elements section")]
+public class ElementsTest
+{
+    [Test]
+    public void ElementsTestSimplePasses()
+    {
+        
+    }
+
+    [UnityTest]
+    public IEnumerator ElementsTestWithEnumeratorPasses()
+    {
+        yield return null;
+    }
+}
